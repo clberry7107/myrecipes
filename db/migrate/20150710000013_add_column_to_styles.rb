1c5e9f4ac99a1d79f1afbea0b9e91a30dbf35643
Chris Berry <clberry7107@yahoo.com>
2015-07-14 18:41:07 +0000

    delete first migration

--- db/migrate/20150710000013_add_column_to_styles.rb
+++
@@ -1,5 +0,0 @@
-class AddColumnToStyles < ActiveRecord::Migration
-  def change
-    add_column :styles, :name, :string
-  end
-end
--- db/migrate/20150710010452_fix_ingredient_columns.rb
+++
@@ -1,6 +0,0 @@
-class FixIngredientColumns < ActiveRecord::Migration
-  def change
-    rename_column :recipe_ingredients, :recipe, :recipe_id
-    rename_column :recipe_ingredients, :ingredient, :ingredient_id
-  end
-end
